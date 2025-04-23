603ee3c2c5daa071ba92795b48fc751184cb1f3e
Ray Smets <rayjsmets@gmail.com>
2025-04-23 13:09:17 -0700

    cleanup

--- bin/install.cjs
+++ bin/install.cjs
@@ -13,12 +13,12 @@ const color = {
   bold: (text) => `\x1b[1m${text}\x1b[0m`
 };
 
-// Display welcome message
-console.log('\n' + color.green('🎉 bot0x installed successfully! 🎉') + '\n');
-console.log(color.bold('To enable shell integration, add these lines to your .zshrc or .bashrc:') + '\n');
-console.log(color.cyan('# Set up commands that modify your shell environment'));
-console.log(color.cyan('eval "$(bot0x shellScript --print)"') + '\n');
-console.log(color.cyan('# Set up all other commands as shell aliases'));
-console.log(color.cyan('eval "$(bot0x alias)"') + '\n');
-console.log(color.bold('For more information, run:') + ' ' + color.yellow('bot0x --help') + '\n');
-console.log(color.bold('Documentation:') + ' ' + color.blue('https://github.com/rsmets/bot0x-cli') + '\n');
+// Force output to be visible even when npm might suppress it
+process.stderr.write('\n' + color.green('🎉 bot0x installed successfully! 🎉') + '\n\n');
+process.stderr.write(color.bold('To enable shell integration, add these lines to your .zshrc or .bashrc:') + '\n\n');
+process.stderr.write(color.cyan('# Set up commands that modify your shell environment\n'));
+process.stderr.write(color.cyan('eval "$(bot0x shellScript --print)"') + '\n\n');
+process.stderr.write(color.cyan('# Set up all other commands as shell aliases\n'));
+process.stderr.write(color.cyan('eval "$(bot0x alias)"') + '\n\n');
+process.stderr.write(color.bold('For more information, run:') + ' ' + color.yellow('bot0x --help') + '\n\n');
+process.stderr.write(color.bold('Documentation:') + ' ' + color.blue('https://github.com/rsmets/bot0x-cli') + '\n\n');
